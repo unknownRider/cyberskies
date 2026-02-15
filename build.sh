2a17478fabba707739f7b2134ddbefcbba13b023
unknownRider <03leafy.tawnier@icloud.com>
2026-02-15 02:05:28 -0700

    Update build.sh
    fixes

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 HUGO_VERSION=0.155.3
 
+echo "Initializing submodules..."
+git submodule update --init --recursive
+
 echo "Installing Hugo ${HUGO_VERSION}..."
 curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
 tar -xf "hugo_${HUGO_VERSION}_linux-amd64.tar.gz" hugo
