4a6382087cf4fada0178aa9eb7793fd50c2676b5
unknownRider <03leafy.tawnier@icloud.com>
2026-02-15 01:57:42 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+HUGO_VERSION=0.155.3
+
+echo "Installing Hugo ${HUGO_VERSION}..."
+curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+tar -xf "hugo_${HUGO_VERSION}_linux-amd64.tar.gz" hugo
+rm "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+
+echo "Hugo version: $(./hugo version)"
+
+echo "Building site..."
+./hugo --gc --minify
